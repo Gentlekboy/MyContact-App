8b87c3e58c65b20477a3700303dfe4efe65192f0
gentlekboy <gentlekboy@gmail.com>
2021-08-09 02:54:56 +0100

    Added first and second implementation activities

---
+++ app/src/main/java/com/gentlekboy/mycontactapp/firstImplementation/ContactList.kt
@@ -0,0 +1,12 @@
+package com.gentlekboy.mycontactapp.firstImplementation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gentlekboy.mycontactapp.R
+
+class ContactList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_list)
+    }
+}
---
+++ app/src/main/java/com/gentlekboy/mycontactapp/secondImplementation/ContactReader.kt
@@ -0,0 +1,12 @@
+package com.gentlekboy.mycontactapp.secondImplementation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gentlekboy.mycontactapp.R
+
+class ContactReader : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_reader)
+    }
+}
