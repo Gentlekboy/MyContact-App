aa100d5cb60e4c71f8322fa4626dfa33b4fcca7f
gentlekboy <gentlekboy@gmail.com>
2021-08-11 01:57:30 +0100

    Added isDeleted variable to know when a contact has been deleted

--- app/src/main/java/com/gentlekboy/mycontactapp/firstImplementation/data/ContactsData.kt
+++ app/src/main/java/com/gentlekboy/mycontactapp/firstImplementation/data/ContactsData.kt
@@ -9,5 +9,26 @@ data class ContactsData(
     var lastName: String? = null,
     var phoneNumber: String? = null,
     var email: String? = null,
-    var abbreviation: String? = null
-    )
+    var abbreviation: String? = null,
+    @get: Exclude
+    var isDeleted: Boolean = false
+    ){
+    override fun equals(other: Any?): Boolean {
+        return if (other is ContactsData){
+            other.id == id
+        }else{
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (firstName?.hashCode() ?: 0)
+        result = 31 * result + (lastName?.hashCode() ?: 0)
+        result = 31 * result + (phoneNumber?.hashCode() ?: 0)
+        result = 31 * result + (email?.hashCode() ?: 0)
+        result = 31 * result + (abbreviation?.hashCode() ?: 0)
+        result = 31 * result + isDeleted.hashCode()
+        return result
+    }
+}
