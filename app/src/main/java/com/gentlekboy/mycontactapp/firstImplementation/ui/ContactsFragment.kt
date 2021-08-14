915aca7bc3a002509139a8c2006a61343fedd30e
gentlekboy <gentlekboy@gmail.com>
2021-08-14 05:46:28 +0100

    Optimized imports

--- app/src/main/java/com/gentlekboy/mycontactapp/firstImplementation/ui/ContactsFragment.kt
+++ app/src/main/java/com/gentlekboy/mycontactapp/firstImplementation/ui/ContactsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
